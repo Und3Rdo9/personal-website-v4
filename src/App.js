717d60f29553043a03d2f63ab0f6a8cc33f33157
Tim Bakkum <tim@bikkelhart.com>
2018-05-10 23:30:53 +0200

    adds a working first call to the graphQL api to test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
+
+const GET_POST = gql`
+query {
+  Post(id:"cjh0yzii63p4s01117pxj9hi6") {
+    title
+    authors {
+      name
+    }
+    dateAndTime
+    postsummary
+    tags
+  }
+}
+`;
+
 class App extends Component {
   render() {
     return (
@@ -11,11 +28,24 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <main>
-          {/* Let's load a post here to test the Graph CMS API */}
-        </main>
-      </div>
-    );
-  }
-}
+          <Query query={GET_POST}>
+            {({ loading, error, data }) => {
+              if (loading) return <div>Loading...</div>;
+              if (error) return <div>Error :(</div>;
 
-export default App;
+                return (
+                  <article>
+                    <h1>{data.Post.title}</h1>
+                    <p>{`by ${data.Post.authors[0].name}`}</p>
+                    <p>{data.Post.postsummary}</p>
+                  </article>
+                )
+              }}
+            </Query>
+          </main>
+        </div>
+      );
+    }
+  }
+
+  export default App;
