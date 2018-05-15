8d4d02f5b1e3e11fd232f48abac31801bf604b32
Tim Bakkum <tim@bikkelhart.com>
2018-05-15 16:53:56 +0200

    adds .navigation styling and switches out Links for NavLinks for active route styling

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => (
-  <nav className="navigation">
-    <ul className="navigation__list">
-      <li className="navigation__item">
-        <Link className="navigation__link" to='/'>Home</Link>
+  <nav className='navigation'>
+    <ul className='navigation__list'>
+      <li className='navigation__item'>
+        <NavLink
+          className='navigation__link'
+          activeClassName='navigation__link--active'
+          exact={true}
+          to='/'
+
+        >
+          Home
+        </NavLink>
       </li>
-      <li className="navigation__item">
-        <Link className="navigation__link" to='/amsterdam-frontend-developer/about'>About me</Link>
+      <li className='navigation__item'>
+        <NavLink
+          className='navigation__link'
+          to='/amsterdam-frontend-developer/about'
+          activeClassName='navigation__link--active'
+          exact={true}
+        >
+          About me
+        </NavLink>
       </li>
-      <li className="navigation__item">
-        <Link className="navigation__link" to='/frontend-blog'>Blog</Link>
+      <li className='navigation__item'>
+        <NavLink
+          className='navigation__link'
+          to='/frontend-blog'
+          activeClassName='navigation__link--active'
+          exact={true}
+        >
+          Blog
+        </NavLink>
       </li>
-      <li className="navigation__item">
-        <Link className="navigation__link" to='/amsterdam-frontend-developer/about'>Contact</Link>
+      <li className='navigation__item'>
+        <NavLink
+          className='navigation__link'
+          to='/amsterdam-frontend-developer/contact'
+          activeClassName='navigation__link--active'
+          exact={true}
+        >
+          Contact
+        </NavLink>
       </li>
     </ul>
   </nav>
