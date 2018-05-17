4f2e58c5827e2c3776d31e203115679cd6cd9357
Tim Bakkum <tim@bikkelhart.com>
2018-05-17 16:22:50 +0200

    corrects className attribute in Hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -9,7 +9,7 @@ export class Hero extends Component { // eslint-disable-line react/prefer-statel
     return (
       <div className='hero'>
         <h1 className='hero__title'>{this.props.title}</h1>
-        <figure class="hero__figure">
+        <figure className="hero__figure">
           <img className='hero__img' src={this.props.imageSrc} alt='Tim Bakkum'/>
         </figure>
         <p className='hero__underline'>{this.props.underline}</p>
