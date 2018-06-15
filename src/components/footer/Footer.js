97dcdac58457af4b5db46575cfc7a774ecc655cf
Tim Bakkum <timbakkum@gmail.com>
2018-06-15 11:32:22 +0200

    adds links to social profiles in footer, corrects link markup

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,38 +6,59 @@ import twitterLogo from './../../assets/images/twitter.svg';
 import githubLogo from './../../assets/images/github.svg';
 
 const Footer = () => (
-  <footer className='app__footer footer'>
+  <footer className="app__footer footer">
     <Container>
-      <section className='footer__contents'>
-        <h5>
-          Elsewhere on the web
-        </h5>
-        <ul className='unstyled-list unstyled-list--horizontal'>
+      <section className="footer__contents">
+        <h5>Elsewhere on the web</h5>
+        <ul className="unstyled-list unstyled-list--horizontal">
           <li>
-            <a href="" target='_blank'>
-              <img src={linkedInLogo} className='footer__icon' />
-              <span className='sr-only'>LinkedIn profile</span>
+            <a
+              href="https://www.linkedin.com/in/tbakkum/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedInLogo} className="footer__icon" alt="" />
+              <span className="sr-only">LinkedIn profile</span>
             </a>
           </li>
           <li>
-            <a href="">
-              <img src={githubLogo} className='footer__icon' />
-              <span className='sr-only'>Github profile</span>
+            <a
+              href="https://github.com/Und3Rdo9/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={githubLogo} className="footer__icon" alt="" />
+              <span className="sr-only">Github profile</span>
             </a>
           </li>
           <li>
-            <a href="">
-              <img src={twitterLogo} className='footer__icon' />
-              <span className='sr-only'>Twitter profile</span>
+            <a
+              href="https://twitter.com/TimBakk_"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={twitterLogo} className="footer__icon" alt="" />
+              <span className="sr-only">Twitter profile</span>
             </a>
           </li>
         </ul>
 
         <h5>Website made with the help of: </h5>
-        <a href='https://graphcms.com/' title='GraphCMS Website' target='_blank'>
-          <img src={graphCmsLogo} alt='GraphCMS Logo' />
+        <a
+          href="https://graphcms.com/"
+          title="GraphCMS Website"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={graphCmsLogo} alt="GraphCMS Logo" />
         </a>
-        <a href='http://sophieshand.com' target='_blank'>Sophie Shand - Graphic Designer</a>
+        <a
+          href="http://sophieshand.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sophie Shand - Graphic Designer
+        </a>
         <p>Copyright &copy; Tim Bakkum 2018</p>
       </section>
     </Container>
