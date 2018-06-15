56182ceba26af12703ce787ebae7de6c9a83b1ce
Tim Bakkum <timbakkum@gmail.com>
2018-06-15 11:36:55 +0200

    turns Hero into functional component

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 /**
  * Hero
  */
-export class Hero extends Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <div className='hero'>
-        <h1 className='hero__title'>{this.props.title}</h1>
-        <figure className="hero__figure">
-          <img className='hero__img' src={this.props.imageSrc} alt='Tim Bakkum'/>
-        </figure>
-        <p className='hero__underline'>{this.props.underline}</p>
-      </div>
-    );
-  }
-}
+export const Hero = props => (
+  <div className="hero">
+    <h1 className="hero__title">{props.title}</h1>
+    <figure className="hero__figure">
+      <img className="hero__img" src={props.imageSrc} alt="Tim Bakkum" />
+    </figure>
+    <p className="hero__underline">{props.underline}</p>
+  </div>
+);
 
 Hero.propTypes = {
   title: PropTypes.string.isRequired,
   underline: PropTypes.string.isRequired,
   imageSrc: PropTypes.string.isRequired
-}
+};
 
 export default Hero;
